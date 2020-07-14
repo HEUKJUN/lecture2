b86924e2e95d3c9b0c7b1b4bdc3c6dfd5019e78b
HYUNJUN <reset0331@gmail.com>
2020-07-14 17:41:28 +0900

    first commit

---
+++ print_str.py
@@ -0,0 +1,5 @@
+def print_str(n):
+    for _ in range(n):
+        print("**************")
+
+print_str(3)
---
+++ sum_func.py
@@ -0,0 +1,6 @@
+def print_sum(a,b) :
+    result = a + b
+    print(a,"과",b,"의 합은",result,"입니다.")
+
+print_sum(1,2)
+print_sum(100,200)
